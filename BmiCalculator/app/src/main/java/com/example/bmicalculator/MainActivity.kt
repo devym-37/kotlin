9639f6e3dc3443e0ee98962436ba80405ad3a63b
David <david@zamface.co.kr>
2022-01-18 12:56:48 +0900

    intent 데이터 전달

--- BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,17 +1,42 @@
 package com.example.bmicalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
+
 // viewBinding
 
 class MainActivity : AppCompatActivity() {
+    private fun saveData(height: Int, weight: Int){
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+
+        editor.putInt("KEY_HEIGHT", height)
+            .putInt("KEY_WEIGHT", weight)
+            .apply()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        resultButton.setOnClickListner {
-            //  결과 버튼이 클릭되면 실
+        resultButton.setOnClickListener {
+            //  결과 버튼이 클릭되면 실행
+            // val intent = Intent(this, ResultActivity::class.java)
+            // intent.putExtra("weight", weightEditText.text.toString())
+            // intent.putExtra("height", heightEditText.text.toString())
+
+            // startActivity(intent)
+
+            startActivity<ResultActivity>(
+                "weight" to weightEditText.text.toString(),
+                "height" to heightEditText.text.toString()
+            )
         }
     }
+
+
 }
--- BmiCalculator/app/src/main/java/com/example/bmicalculator/ResultActivity.kt
+++ BmiCalculator/app/src/main/java/com/example/bmicalculator/ResultActivity.kt
@@ -2,10 +2,39 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result.*
+import org.jetbrains.anko.toast
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        // 전달받은 키 / 몸무게
+        val height = intent.getStringExtra("height")?.toInt()
+        val weight = intent.getStringExtra("weight")?.toInt()
+
+        // BMI 계산
+        val bmi = weight!! / Math.pow(height!! / 100.0, 2.0)
+
+        // 결과 표시
+        when {
+            bmi >= 35 -> resultTextView.text = "고도 비만"
+            bmi >= 30 -> resultTextView.text = "2단계 비반"
+            bmi >= 25 -> resultTextView.text = "1단계 비반"
+            bmi >= 23 -> resultTextView.text = "과체중"
+            bmi >= 18.5 -> resultTextView.text = "정상"
+            else -> resultTextView.text = "저체중"
+        }
+
+
+        // 이미지 표시
+        when {
+            bmi >= 23 -> imageView.setImageResource(R.drawable.ic_baseline_sentiment_dissatisfied_24)
+            bmi >= 18.5 -> imageView.setImageResource(R.drawable.ic_baseline_sentiment_satisfied_24)
+            else -> imageView.setImageResource(R.drawable.ic_baseline_sentiment_very_dissatisfied_24)
+        }
+
+        toast("$bmi")
     }
 }
