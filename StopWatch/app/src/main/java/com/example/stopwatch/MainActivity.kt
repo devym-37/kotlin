3ed6dce6dc92db5e30e4e0ed0dae38c66d97b840
Dev-miniyoung <miniyoung37@gmail.com>
2022-01-26 12:58:01 +0900

    stopWatch 작업

--- StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import java.util.*
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
+    private var time = 0
+    private var isRunning = false
+    private var timerTask: Timer? = null
+    private var fab: View = findViewById(R.id.floatingActionButton)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener {
+            isRunning = !isRunning
+
+            if (isRunning) {
+                start()
+            } else {
+                pause()
+            }
+        }
+    }
+
+
+
+    private fun start() {
+        fab.setImageResource(R.drawable.ic_baseline_pause_24)
+
+        timerTask = timer(period = 10){
+            time++
+            val sec = time / 100
+            val millisec = time % 100
+            runOnUiThread {
+                secTextView.text = "$sec"
+                milliTextView.text = "$millisec"
+            }
+        }
+    }
+
+    private fun pause() {
+        fab.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+        timerTask?.cancel()
     }
 }
