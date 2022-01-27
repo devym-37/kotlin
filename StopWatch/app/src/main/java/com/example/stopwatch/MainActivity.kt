e279aa56ada5f9743808ee4f0759bc4805887a6e
Dev-miniyoung <miniyoung37@gmail.com>
2022-01-27 12:59:24 +0900

    StopWatch project completed

--- StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -3,14 +3,16 @@ package com.example.stopwatch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import java.util.*
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
     private var time = 0
     private var isRunning = false
     private var timerTask: Timer? = null
-    private var fab: View = findViewById(R.id.floatingActionButton)
+    private var lap = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +26,14 @@ class MainActivity : AppCompatActivity() {
             } else {
                 pause()
             }
+        }
+
+        lapButton.setOnClickListener {
+            recordLapTime()
+        }
+
+        resetFab.setOnClickListener {
+            reset()
         }
     }
 
@@ -46,5 +56,28 @@ class MainActivity : AppCompatActivity() {
     private fun pause() {
         fab.setImageResource(R.drawable.ic_baseline_play_arrow_24)
         timerTask?.cancel()
+    }
+
+    private fun reset() {
+        timerTask?.cancel()
+
+        time = 0
+        isRunning = false
+        fab.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+        secTextView.text = "0"
+        milliTextView.text = "00"
+
+        lapLayout.removeAllViews()
+        lap = 1
+    }
+
+    private fun recordLapTime() {
+        val lapTime = this.time
+        val textView = TextView(this)
+        textView.text = "$lap LAP : ${lapTime / 100}.${lapTime % 100}"
+
+        // 맨위에 랩타임 추가
+        lapLayout.addView(textView, 0)
+        lap++
     }
 }
