fe26a07823dba1a9f110a91cb4e73a0a468a0c53
miniyoung <miniyoung37@gmail.com>
2022-01-31 16:37:21 +0900

    03WebBrowser webview 추가 및 버튼 추가

--- WebBrowser/app/src/main/java/com/example/webbrowser/MainActivity.kt
+++ WebBrowser/app/src/main/java/com/example/webbrowser/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.webbrowser
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 웹뷰 기본설정
+        webView.apply {
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+        }
+
+        webView.loadUrl("http://www.google.com")
+
+        urlEditText.setOnEditorActionListener {_, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                webView.loadUrl(urlEditText.text.toString())
+                true
+            } else {
+                false
+            }
+        }
     }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
 }
