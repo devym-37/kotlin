d1f4d9bdd5997fe6612e941ba861dc393e6104fc
miniyoung <miniyoung37@gmail.com>
2022-02-03 00:21:53 +0900

    04TiltSensor init

---
+++ TiltSensor/app/src/main/java/com/example/tiltsensor/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.tiltsensor
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private val sensorManager by lazy {
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+
+
+}
